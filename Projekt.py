43c0af1529eaf590fb72755fd8331f8eeda19507
icecreamik <62022530+icecreamik@users.noreply.github.com>
2020-04-09 10:57:11 -0700

    Add files via upload

---
+++ Projekt.py
@@ -0,0 +1,11 @@
+i = 0
+mesta=[]
+mesta[1][1]= 1
+print(mesta)
+pocet_mest=int(input("Kolik měst budeme porovnávat? \t"))
+while i < pocet_mest:
+    zadane_mesto_x_retezec = int(input("Zadejte souradnici x \t"))
+    zadane_mesto_y_retezec = int(input("Zadejte souradnici y  \t"))
+    mesta.append()
+    i=i+1
+print(mesta)
